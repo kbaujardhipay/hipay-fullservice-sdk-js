f1ae06f797f8b95ea154c65a717a5128244e78c5
Unknown <jimmy.kurc@gmail.com>
2017-07-28 17:27:58 +0200

    tokenize bugfix for IE < 10

--- example/setup.sh
+++ example/setup.sh
@@ -1,15 +1,15 @@
-cd example
+#cd example
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '$(curl -q https://composer.github.io/installer.sig)') { echo 'Installer verified' . PHP_EOL; } else { echo 'Installer corrupt' . PHP_EOL;}"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php -r "if (hash_file('SHA384', 'composer-setup.php') === '$(curl -q https://composer.github.io/installer.sig)') { echo 'Installer verified' . PHP_EOL; } else { echo 'Installer corrupt' . PHP_EOL;}"
+#php composer-setup.php
+#php -r "unlink('composer-setup.php');"
 
-php composer.phar install
+#php composer.phar install
 
-cd ..
-
-npm install
+#cd ..
+docker-compose up -d
+docker run --rm -v $(pwd)/etc/ssl:/certificates -e "SERVER=localhost" jacoelho/generate-certificate
 
 echo ""
 echo "Setup completed! Do not forget to add your HiPay Fullservice credentials into the example/credentials.php file."
