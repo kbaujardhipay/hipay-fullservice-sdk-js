7b80bb2b7c882541d53f1574b2d45c43f5cce2fa
Unknown <jimmy.kurc@gmail.com>
2017-12-05 18:19:58 +0100

    Debug  FF key left right

--- dist/hipay-fullservice-sdk-2.js
+++ dist/hipay-fullservice-sdk-2.js
@@ -4692,15 +4692,13 @@ var HiPay = (function (HiPay) {
             if (propt == 'cardNumber') {
                 // cardNumber keydown
                 var cardNumberHandlerKeydown = function (e) {
+
                     var evt = e || window.event;
                     var charCode = evt.keyCode || evt.which;
                     if (charCode == 8 || charCode == 46) {
                         _instanceServiceCreditCard = new _serviceCreditCard();
                         _instanceServiceCreditCard.initCreditCardNumber(charCode);
-
                         evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
-
-                    } else {
 
                     }
                     _callbackEventFormChange();
@@ -4727,18 +4725,26 @@ var HiPay = (function (HiPay) {
                 _addFieldListener(_selectElementWithHipayId(_idInputMapper[propt]), 'paste', cardNumberHandlerPaste, false);
                 // ./ cardNumber paste
 
-                // cardNumber keypress
+                // cardNumber keypress not handle in FF
                 var cardNumberHandlerKeypress = function (e) {
                     var evt = e || window.event;
                     var charCode = evt.keyCode || evt.which;
-                    evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
-                    if (charCode >= 48 && charCode <= 57) {
-                        /* is valid add char */
-                        _instanceServiceCreditCard = new _serviceCreditCard();
-                        _instanceServiceCreditCard.initCreditCardNumber(charCode);
+
+                    if (charCode == 8 || charCode == 46) {
+
+                    } else {
+                        if (typeof evt.key != "undefined" && evt.key.length === 1) {
+
+                            if (charCode >= 48 && charCode <= 57) {
+                                /* is valid add char */
+                                _instanceServiceCreditCard = new _serviceCreditCard();
+                                _instanceServiceCreditCard.initCreditCardNumber(charCode);
+                                evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+                            }
+                        }
+                        _callbackEventFormChange();
                     }
-                    _callbackEventFormChange();
-                }
+                };
                 _addFieldListener(_selectElementWithHipayId(_idInputMapper[propt]), 'keypress', cardNumberHandlerKeypress, false);
                 // ./ cardNumber keypress
 
@@ -4766,7 +4772,6 @@ var HiPay = (function (HiPay) {
                         _instanceServiceCreditCard = new _serviceCreditCard();
                         _instanceServiceCreditCard.initCreditCardHolder(charCode);
                         evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
-
                     }
                     _callbackEventFormChange();
                 };
@@ -4779,10 +4784,13 @@ var HiPay = (function (HiPay) {
                     var evt = e || window.event;
                     var charCode = evt.keyCode || evt.which;
                     if (charCode == 8 || charCode == 46) {
+
                     } else {
-                        _instanceServiceCreditCard = new _serviceCreditCard();
-                        _instanceServiceCreditCard.initCreditCardHolder(charCode);
-                        evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+                        if (typeof evt.key != "undefined" && evt.key.length === 1) {
+                            _instanceServiceCreditCard = new _serviceCreditCard();
+                            _instanceServiceCreditCard.initCreditCardHolder(charCode);
+                            evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+                        }
                     }
                     _callbackEventFormChange();
                 };
@@ -5688,35 +5696,35 @@ var HiPay = (function (HiPay) {
                     }
 
                 })
-            .catch(function (error) {
-                // retry call
-                fetch(endpoint, {
-                    method: "POST",
-                    headers: config['headers'],
-                    body: JSON.stringify( requestParams )
-                })
-                    .then(function (response) {
-                        return response.json();
+                .catch(function (error) {
+                    // retry call
+                    fetch(endpoint, {
+                        method: "POST",
+                        headers: config['headers'],
+                        body: JSON.stringify( requestParams )
                     })
-                    .then(function (result) {
-                        if( typeof result['code'] != 'undefined' )  {
-                            reject(new _APIError(result));
-                        }
-                        else {
-                            var cardToken = new HiPay.Token(result);
-                            cardToken.constructor.populateProperties(cardToken,result);
-                            _disableAllInput();
-                            resolve(cardToken);
+                        .then(function (response) {
+                            return response.json();
+                        })
+                        .then(function (result) {
+                            if( typeof result['code'] != 'undefined' )  {
+                                reject(new _APIError(result));
+                            }
+                            else {
+                                var cardToken = new HiPay.Token(result);
+                                cardToken.constructor.populateProperties(cardToken,result);
+                                _disableAllInput();
+                                resolve(cardToken);
 
-                        }
+                            }
 
-                    })
-                    .catch(function (error) {
-                        reject(new _APIError(error));
+                        })
+                        .catch(function (error) {
+                            reject(new _APIError(error));
 
-                    });
+                        });
 
-            });
+                });
 
         });
 
